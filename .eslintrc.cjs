6c8e70a2cb68d92d857625888cee807822f10160
jydoskey <jideomotola33@gmail.com>
2022-03-16 11:06:14 +0100

    migrated from typescript to javascript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,21 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
-
 module.exports = {
-  root: true,
-  extends: [
+  "root": true,
+  "extends": [
     "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
+    "eslint:recommended"
   ],
-  env: {
-    "vue/setup-compiler-macros": true,
+  "env": {
+    "vue/setup-compiler-macros": true
   },
-  overrides: [
+  "overrides": [
     {
-      files: ["cypress/integration/**.spec.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
-    },
-  ],
-};
+      "files": [
+        "cypress/integration/**.spec.{js,ts,jsx,tsx}"
+      ],
+      "extends": [
+        "plugin:cypress/recommended"
+      ]
+    }
+  ]
+}
