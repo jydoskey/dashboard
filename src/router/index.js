db8b84a0761de5770326872339e9501d844889b8
jydoskey <jideomotola33@gmail.com>
2022-03-17 18:20:07 +0100

    declared vue routes for pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+
+const routes = [
+	{
+    path: '/',
+    name: 'DashboardSignin',
+    component: () => import('@/components/signIn.vue')
+  },
+  {
+    path: '/sign-up',
+    name: 'DashboardSignup',
+    component: () => import('@/components/signUp.vue')
+  }
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    
-  ]
+  routes,
 })
 
 export default router
