463fb1733c5fc1add0e3edf561cdc15f9c4005ef
jydoskey <jideomotola33@gmail.com>
2022-03-23 17:43:18 +0100

    home file holder for sidebar and dashboard

---
+++ src/components/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <sidebar></sidebar>
+    <dashboard></dashboard>
+  </div>
+</template>
+
+<script>
+import sidebar from "@/components/dashboard/sidebar.vue";
+import dashboard from "@/components/dashboard/dashboard.vue";
+export default {
+  name: "DashboardHomepage",
+
+  data() {
+    return {};
+  },
+
+  components: {
+    dashboard,
+    sidebar,
+  },
+};
+</script>
